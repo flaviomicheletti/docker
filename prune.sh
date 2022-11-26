8eb433775461ee5bbc1c7bcbd384fcd430767366
Flávio Micheletti <flaviomicheletti@hotmail.com>
2022-11-26 14:27:13 -0300

    prune.sh

---
+++ prune.sh
@@ -0,0 +1,9 @@
+docker stop $(docker ps -a -q)
+docker container rm $(docker container ls -qa)
+docker container prune
+
+docker image rm $(docker image ls -aq)
+docker image prune
+
+docker volume prune
+docker network prune
